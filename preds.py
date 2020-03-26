ef4eebccd93e23ac8cac14d6eb4d36063bbe5c29
Chris Wang <cwang7@caltech.edu>
2020-03-26 17:25:32 -0400

    Patch preds to use temps, ignore data dir
    Signed-off-by: Chris Wang <cwang7@caltech.edu>

--- preds.py
+++ preds.py
@@ -5,23 +5,36 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.utils import to_categorical
 from keras_preprocessing.sequence import pad_sequences
 
-from rnn import build_data
+from rnn import build_data, build_model
 
-_, _,ctoi,  itoc = build_data("data/shakespeare.txt", 40, 3)
-model = load_model("trained_checkpoints/weights-improvement-68-0.0014.hdf5")
-
-seed = "shall i compare thee to a summer's day\n"
-
-result = ""
-for _ in range(1000):
-    seq = [ctoi[c] for c in seed]
-    enc = pad_sequences([seq], maxlen=40, truncating='pre')
-
-    enc = to_categorical(enc, num_classes=len(ctoi))
-    enc = enc.reshape(1, 40, 30)
-
-    p_ind = model.predict_classes(enc)
-    c = itoc[p_ind[0]]
-    seed += c
-    # seed = seed[1:len(seed)]
-print(seed)
+# Build dataset for mapping dicts and shapes.
+X, _,ctoi,  itoc = build_data("data/shakespeare.txt", 40, 3)
+# Load the trained model from the model file.
+trained_model = load_model("trained_checkpoints/weights-improvement-68-0.0014.hdf5")
+# For the set of temperatures specified
+for t in [0.25, 0.75, 1.5]:
+    # Initial seed
+    seed = "shall i compare thee to a summer's day\n"
+    # Build model with specified temp
+    model = build_model((40, len(X[0][0])), temperature=t)
+    # Get trained weights
+    model.set_weights(trained_model.get_weights())
+    print("500 character prediction for temperature {}.".format(t))
+    # Predict 500 Characters
+    for _ in range(500):
+        # Convert to ints
+        seq = [ctoi[c] for c in seed]
+        # Pad to 40 characters
+        enc = pad_sequences([seq], maxlen=40, truncating='pre')
+        # Convert to categorical encoding
+        enc = to_categorical(enc, num_classes=len(ctoi))
+        # Reshape to fit the network
+        enc = enc.reshape(1, 40, 30)
+    
+        # Predict an integer
+        p_ind = model.predict_classes(enc)
+        # Convert back to character
+        c = itoc[p_ind[0]]
+        # Append to seed
+        seed += c
+    print(seed)
